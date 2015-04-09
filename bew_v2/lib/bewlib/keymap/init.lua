f2fd2a01b5827ddfdc24bf0e4600489f3a6f10b2
lesell_b <lesell_b@epitech.eu>
2015-04-09 02:16:54 +0200

    bew: add apply flags && setModifiers (basic) && change intern table name

--- bew_v2/lib/bewlib/keymap/init.lua
+++ bew_v2/lib/bewlib/keymap/init.lua
@@ -1,25 +1,65 @@
 -- Module dependencies
+local awful = require("awful")
+
 local toast = require("bewlib.utils").toast
 
 -- Module environement
-local mod = {}
+local keymap = {}
 
-function mod.new (keymapName, options)
+--[[ Apply flags ]]--
+keymap.SET = 0x1
+keymap.MERGE = 0x2
+keymap.KEYGRABBER = 0x3
+
+function keymap.new (keymapName, options)
 	self.name = keymapName
 	self.parent = options.parent or nil
+	self.binds = {}
+	self.modifiers = {}
 end
 
+--[[ call: 
+keymap:setModifiers( {
+	"M" = modkey,
+	"S" = "Shift"
+})
+--]]
+function keymap:setModifiers (modifiersTable)
+	if type(modifiersTable) == "table" then
+		self.modifiers = modifiersTable
+		return self -- to chain commands (or return modifiersTable)
+	end
+	return nil
+end
 
-function mod:addBind (bindOpt)
+function keymap:addBind (bindOpt)
 	if type(bindOpt) ~= "table" then
 		return nil
 	end
 	local bind = {}
+	bind.ctrl = type(bindOpt.ctrl) == "table" and bindOpt.ctrl or nil
+	if bind.ctrl == nil then
+		return nil
+	end
+
 	bind.comment = type(bindOpt.comment) == "string" and bindOpt.comment or ""
+	bind.hashtags = type(bindOpt.hashtags) == "string" and bindOpt.hashtags or ""
 	bind.cmd = type(bindOpt.cmd) == "string" and bindOpt.cmd or nil
 	bind.callback = type(bindOpt.cmd) == "function" and bindOpt.cmd or nil
+
+	table.insert(self.binds, bind)
+	return bind
 end
 
 
 
-return mod
+
+function keymap:apply(flag)
+	if flag == keymap.SET then -- replace the current keymap
+	else if flag == keymap.MERGE then -- add to the current keymap
+	else if flag == keymap.KEYGRABBER then -- replace like in keygrabber (for a moment)
+	end
+end
+
+
+return keymap
