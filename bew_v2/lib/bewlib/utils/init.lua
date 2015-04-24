179119c9008347711828f76c1ef1f3e5b20764bd
lesell_b <lesell_b@epitech.eu>
2015-04-24 12:44:59 +0200

    bew: add setTimeout and setInterval (based on timer)

--- bew_v2/lib/bewlib/utils/init.lua
+++ bew_v2/lib/bewlib/utils/init.lua
@@ -1,13 +1,35 @@
+-- Grab environement
+local capi = {
+	timer = timer
+}
+
 -- Module dependencies
 local naughty = require("naughty")
 
 -- Module environement
 local mod = {}
 
-function mod.toast (text, options)
+function mod.toast(text, options)
 	local options = options or {}
 	options.text = text
-	naughty.notify(options)
+	return naughty.notify(options)
+end
+
+function mod.setTimeout(callback, timeout)
+	local theTimer = capi.timer({ timeout = timeout })
+	theTimer:connect_signal("timeout", function()
+		theTimer:stop()
+		callback()
+	end)
+	theTimer:start()
+	return theTimer
+end
+
+function mod.setInterval(callback, interval)
+	local theTimer = capi.timer({ timeout = interval })
+	theTimer:connect_signal("timeout", callback)
+	theTimer:start()
+	return theTimer
 end
 
 return mod
