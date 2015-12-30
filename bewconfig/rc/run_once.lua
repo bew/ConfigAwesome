f4d5c156c67ebed5ed78936e1f21e3f1a2557c2f
lesell_b <lesell_b@epitech.eu>
2015-12-30 15:05:55 +0100

    use bewlib.autorun to 'run once' some programs

--- bewconfig/rc/run_once.lua
+++ bewconfig/rc/run_once.lua
@@ -1,38 +1,11 @@
-local awful = require("awful")
+local Autorun = require("bewlib.autorun")
 
-function run_once(cmd)
-	findme = cmd
-	firstspace = cmd:find(" ")
-	if firstspace then
-		findme = cmd:sub(0, firstspace-1)
-	end
-	awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
-end
+Autorun.addOnce("xset r rate 500")
+Autorun.addOnce("xset -b")
+Autorun.addOnce("xbacklight -set 70")
 
-function just_run(cmd)
-	awful.util.spawn_with_shell(cmd)
-end
-
-local function add_x_safe_config(tbl)
-	return awful.util.table.join(tbl, {
-		"xset r rate 500",
-		"xset -b",
-		"xbacklight -set 70"
-	})
-end
-
-local to_run = {
-	--"wpa_gui -t",
-	--"QNetSoul",
-	"chromium --no-startup-window",
-	"amixer set Master 20%",
-}
-
-
-
-to_run = add_x_safe_config(to_run)
-
-for i=1, #to_run do
-	run_once(to_run[i])
-end
+--Autorun.addOnce("wpa_gui -t")
+--Autorun.addOnce("QNetSoul")
+Autorun.addOnce("chromium --no-startup-window")
+Autorun.addOnce("pamixer --set-volume 40")
 
