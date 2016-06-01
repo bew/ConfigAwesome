3a83a43bd2cdd07b4e5cb2974bd99068b743a3b1
lesell_b <lesell_b@epitech.eu>
2016-06-02 01:32:27 +0200

    fix default volume

--- bewconfig/rc/run_once.lua
+++ bewconfig/rc/run_once.lua
@@ -7,5 +7,5 @@ Autorun.addOnce("xbacklight -set 70")
 --Autorun.addOnce("wpa_gui -t")
 --Autorun.addOnce("QNetSoul")
 --Autorun.addOnce("chromium --no-startup-window")
-Autorun.addOnce("pamixer --set-volume 40")
+Autorun.addOnce("pamixer --set-volume 32")
 
