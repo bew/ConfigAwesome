5f173aeb8df37708efa48565dd02dd314fc2e79b
Benoit de Chezelles <lesell_b@epitech.eu>
2018-06-27 19:47:40 -0700

    Comment brightness & audio volume setter on reload

--- bewconfig/rc/run_once.lua
+++ bewconfig/rc/run_once.lua
@@ -2,7 +2,7 @@ local Autorun = require("bewlib.autorun")
 
 Autorun.addOnce("xset r rate 500")
 Autorun.addOnce("xset -b")
-Autorun.addOnce("light -S 42")
 
-Autorun.addOnce("pamixer --set-volume 32")
+--Autorun.addOnce("light -S 42")
+--Autorun.addOnce("pamixer --set-volume 32")
 
